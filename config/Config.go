0f1c131eafd502ba5570ff44a1f8dbeb7f7381d2
ps <15820736375@163.com>
2021-11-24 20:35:35 +0800

    init project

--- config/Config.go
+++ config/Config.go
@@ -1,13 +1,20 @@
 package config
 
 import (
+	"fmt"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"net/url"
 	"os"
 	"path"
-
-	"github.com/spf13/viper"
 )
 
-var APPVIPER *viper.Viper
+var (
+	APPVIPER *viper.Viper
+	DB       *gorm.DB
+)
 
 func InitAppConfig() *viper.Viper {
 	workDir, _ := os.Getwd()
@@ -24,4 +31,34 @@ func InitAppConfig() *viper.Viper {
 
 func init() {
 	APPVIPER = InitAppConfig()
+	DB = InitDB()
+}
+
+func InitDB() *gorm.DB {
+	host := APPVIPER.GetString("database.host")
+	port := APPVIPER.GetString("database.port")
+	database := APPVIPER.GetString("database.databaseName")
+	username := APPVIPER.GetString("database.username")
+	password := APPVIPER.GetString("database.password")
+	charset := APPVIPER.GetString("database.charset")
+	loc := APPVIPER.GetString("database.loc")
+
+	sqlStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=%s",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset,
+		url.QueryEscape(loc),
+	)
+
+	//db, err := gorm.Open(mysql.Open(sqlStr), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	db, err := gorm.Open(mysql.Open(sqlStr), &gorm.Config{Logger: logger.Default.LogMode(logger.Warn)})
+	if err != nil {
+		panic("connected error" + err.Error())
+	} else {
+		fmt.Println("connected db")
+	}
+	return db
 }
--- db/Database.go
+++ db/Database.go
@@ -1,45 +1,17 @@
 package db
 
-import (
-	"SyncEthData/config"
-	"fmt"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"net/url"
-)
+func SaveBlock() {
 
-var DB *gorm.DB
-
-func init() {
-	DB = InitDB()
 }
 
-func InitDB() *gorm.DB {
-	host := config.APPVIPER.GetString("database.host")
-	port := config.APPVIPER.GetString("database.port")
-	database := config.APPVIPER.GetString("database.databaseName")
-	username := config.APPVIPER.GetString("database.username")
-	password := config.APPVIPER.GetString("database.password")
-	charset := config.APPVIPER.GetString("database.charset")
-	loc := config.APPVIPER.GetString("database.loc")
+func SaveHeader() {
 
-	sqlStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=%s",
-		username,
-		password,
-		host,
-		port,
-		database,
-		charset,
-		url.QueryEscape(loc),
-	)
+}
 
-	//db, err := gorm.Open(mysql.Open(sqlStr), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
-	db, err := gorm.Open(mysql.Open(sqlStr), &gorm.Config{Logger: logger.Default.LogMode(logger.Warn)})
-	if err != nil {
-		panic("connected error" + err.Error())
-	} else {
-		fmt.Println("connected db")
-	}
-	return db
+func SaveTrx() {
+
+}
+
+func BatchSaveTrx() {
+
 }
